206d357f16428bc07a721c59701ed7bdc363c00d
James Noleto Brandao <56309987+devjamesbrandao@users.noreply.github.com>
2023-01-30 10:17:56 -0300

    feat: add JWT Bearer Toke  in swagger

--- Autentication.API/Configuration/SwaggerConfig.cs
+++ Autentication.API/Configuration/SwaggerConfig.cs
@@ -11,6 +11,36 @@ namespace Autentication.API.Configuration
                 {
                     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Cancelatin token and Rate Limit", Version = "v1" });
 
+                    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                    {
+                        Description = @"JWT Authorization header usando o padrão Bearer. \r\n\r\n 
+                                    Adicione 'Bearer' [espaço] e então seu token no input abaixo.
+                                    \r\n\r\nExemplo: 'Bearer 12345abcdef'",
+                        Name = "Authorization",
+                        In = ParameterLocation.Header,
+                        Type = SecuritySchemeType.ApiKey,
+                        Scheme = "Bearer"
+                    });
+
+                    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                    {
+                        {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+
+                            },
+                            new List<string>()
+                        }
+                    });
+
                     var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
 
                     var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
